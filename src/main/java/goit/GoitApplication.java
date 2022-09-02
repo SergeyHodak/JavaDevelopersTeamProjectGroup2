3e351132bd71aabf5b309374338c2abd13c7477a
SergeyHodak <87858809+SergeyHodak@users.noreply.github.com>
2022-09-02 11:45:20 +0300

    start

---
+++ src/main/java/goit/GoitApplication.java
@@ -0,0 +1,13 @@
+package goit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goit/GoitApplicationTests.java
@@ -0,0 +1,13 @@
+package goit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
