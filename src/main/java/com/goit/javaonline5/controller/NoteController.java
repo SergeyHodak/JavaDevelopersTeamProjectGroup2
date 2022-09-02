c14719de5007d17c07f7e411ad166a346ce64e11
nikitakordyuk <tto3utiv4ik@gmail.com>
2022-09-02 22:09:54 +0300

    Initial Notes commit.

---
+++ src/main/java/com/goit/javaonline5/controller/NoteController.java
@@ -0,0 +1,65 @@
+package com.goit.javaonline5.controller;
+
+import com.goit.javaonline5.dao.abstraction.NoteDaoService;
+import com.goit.javaonline5.model.NoteModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/note")
+public class NoteController {
+
+    private final NoteDaoService noteDaoService;
+
+    @GetMapping("")
+    public String allNotesPage(Model model) {
+        model.addAttribute("allNotes", noteDaoService.findAll());
+
+        return "note/index";
+    }
+
+    @GetMapping("/new")
+    public String newNotePage(Model model) {
+        model.addAttribute("note", new NoteModel());
+
+        return "note/new";
+    }
+
+    @PostMapping("/new")
+    public String addNewNote(@ModelAttribute NoteModel noteModel) {
+        noteDaoService.save(noteModel);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public String certainNoteIdPage(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("general", noteDaoService.findById(id));
+
+        return "note/show";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editNotePage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("general", noteDaoService.findById(id));
+
+        return "note/edit";
+    }
+
+    @PatchMapping("/{id}/edit")
+    public String editNoteRequest(@PathVariable("id") long id, @ModelAttribute NoteModel noteModel) {
+        noteDaoService.updateById(noteModel, id);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteNote(@PathVariable long id) {
+        noteDaoService.delete(id);
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/goit/javaonline5/dao/abstraction/NoteDaoService.java
@@ -0,0 +1,6 @@
+package com.goit.javaonline5.dao.abstraction;
+
+import com.goit.javaonline5.model.NoteModel;
+
+public interface NoteDaoService extends BaseDaoService<NoteModel> {
+}
