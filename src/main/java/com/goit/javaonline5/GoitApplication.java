47aeb83ee5d4a9eff549b65828a79c6f7929f7af
AnnaHuzii <anna_guzii@ukr.net>
2022-09-03 20:43:29 +0300

    migration_insert_v3

--- src/main/java/com/goit/javaonline5/GoitApplication.java
+++ src/main/java/com/goit/javaonline5/GoitApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoitApplication {
-
 	public static void main(String[] args) {
 		new DbInitService().initDb();
+		SpringApplication.run(GoitApplication.class, args);
 	}
 
 }
