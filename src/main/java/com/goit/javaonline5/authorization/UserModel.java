4053c1f4fc42be90ca40ee7c0ff70382c46ebf37
Максим Баргамин <bargamin123@gmail.com>
2022-09-03 19:15:02 +0300

    added user entity, service and controller

---
+++ src/main/java/com/goit/javaonline5/authorization/UserModel.java
@@ -0,0 +1,54 @@
+package com.goit.javaonline5.authorization;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public UserModel(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserModel() {
+
+    }
+}
+
