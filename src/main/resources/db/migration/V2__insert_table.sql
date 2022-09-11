9f6124227f625911459560a05f705420763e7f42
nikitakordyuk <tto3utiv4ik@gmail.com>
2022-09-11 21:39:22 +0300

    Changed migration

--- src/main/resources/db/migration/V2__insert_table.sql
+++ src/main/resources/db/migration/V2__insert_table.sql
@@ -1,3 +1,3 @@
 INSERT INTO users(id, email, first_name, last_name, password)
 values ('031b67a0-31be-11ed-a261-0242ac120002', 'admin@ukr.net', 'admin', 'admin',
-        '$2a$10$sJhCLqPnUmaGpZjLWD9scOMbIN.M1UPoK.rXZ/7n7wIyx68L56wvy');
+        '$2a$10$sJhCLqPnUmaGpZjLWD9scOMbIN.M1UPoK.rXZ/7n7wIyx68L56wvy') ON CONFLICT DO NOTHING;
